f39df36383e0d717964b4990f262313320926cec
Jon Perl <perl.jonathan@gmail.com>
2019-11-30 14:05:14 -0500

    update fixtures to new target DocSelector

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import { loadEvents } from "./index";
 
 test("loadEvents load events", async () => {
-  const events = await loadEvents("login");
+  const events = await loadEvents("scroll_login");
   expect(events).toBeTruthy();
 });
